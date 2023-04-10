d83835add3bdbcb8a1dd2c6b46c67bb850fb69d8
FAMBL <fambl@softwareoptima.com>
2023-04-10 11:19:58 +0800

    First commit.

---
+++ src/app.controller.ts
@@ -0,0 +1,85 @@
+import {
+  Body,
+  Controller,
+  Get,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import express, { Express } from 'express';
+import { AppService } from './app.service';
+import { SampleDto } from './sample.dto';
+import * as multer from 'multer'
+import { diskStorage } from 'multer'
+import { extname } from 'path';
+const upload = multer({ dest: 'uploads/' })
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  sayHello() {
+    return this.appService.getHello();
+  }
+  
+  // refer to https://stackoverflow.com/questions/49096068/upload-file-using-nestjs-and-multer
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads'  
+    })
+  }))
+  @Post('file')
+  uploadFile(
+    @Body() body: SampleDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) { 
+    return {
+      body,
+      file: file,
+    };
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('file/pass-validation')
+  uploadFileAndPassValidation(
+    @Body() body: SampleDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'json',
+        })
+        .build({
+          fileIsRequired: false,
+        }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return {
+      body,
+      file: file,
+    };
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('file/fail-validation')
+  uploadFileAndFailValidation(
+    @Body() body: SampleDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'jpg',
+        })
+        .build(),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      body,
+      file: file.buffer.toString(),
+    };
+  }
+}
+ 
